a83c1a8dbaaa677fdce34f975a5c11f841ccfa41
josegs72 <josegs72@gmail.com>
2023-02-12 09:49:33 +0100

    change1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { CharacterDetailsComponent } from './pages/character-details/character-details.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharactersComponent,
+    CharacterDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
