e9ef459d013d6238e23f2c75fe39282328fbf500
josegs72 <josegs72@gmail.com>
2023-02-19 17:27:00 +0100

    loading

--- src/app/pages/character-details/character-details.component.ts
+++ src/app/pages/character-details/character-details.component.ts
@@ -1,32 +1,23 @@
-
-
 import { Component } from '@angular/core';
 import { Icharacters } from 'src/app/models/icharacters';
-  
-  import { ActivatedRoute } from '@angular/router';
-  
-  import { characters } from 'src/app/models/characters.data';
- 
+
+import { ActivatedRoute } from '@angular/router';
+
+import { characters } from 'src/app/models/characters.data';
 
 @Component({
   selector: 'app-character-details',
   templateUrl: './character-details.component.html',
-  styleUrls: ['./character-details.component.scss']
+  styleUrls: ['./character-details.component.scss'],
 })
 export class CharacterDetailsComponent {
- 
-  public id! : any;
-  public character? : Icharacters;
-
+  public id!: any;
+  public character?: Icharacters;
 
   constructor(public route: ActivatedRoute) {
-    this.route.paramMap.subscribe((params) => this.id = params.get('id'));
+    this.route.paramMap.subscribe((params) => (this.id = params.get('id')));
     this.id = parseInt(this.id);
     this.character = this.characters.find((person) => person.id === this.id);
-    console.log(this.character);
   }
   public characters: Icharacters[] = characters;
 }
-
-
-
